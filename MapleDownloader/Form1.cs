104c354ff3e7f289b73027de7136e57cfa154aa7
AIP <sagi257@gmail.com>
2016-07-07 12:56:53 +0300

    Update Form1.cs

--- MapleDownloader/Form1.cs
+++ MapleDownloader/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net;
 using System.Threading;
@@ -12,7 +12,9 @@ namespace MapleDownloader
 {
     public partial class Form1 : Form
     {
-        private string filePath, baseUrl, baseName, fullName;
+        private readonly string baseUrl = "http://download2.nexon.net/Game/MapleStory/FullVersion/";
+        private readonly string baseName = "MSSetupv";
+        private string savePath, fullFilePath;
         private bool finishedDownload;
         private int partNum, seconds;
         private WebClient c;
@@ -22,28 +24,25 @@ namespace MapleDownloader
         {
             InitializeComponent();
 
-            baseUrl = "http://download2.nexon.net/Game/MapleStory/FullVersion/";
-
             finishedDownload = true;
 
-            baseName = "MSSetupv";
             partNum = 1;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            versions.Items.Add("62");
+            addSpecialVersions();
             addVersions();
 
             versions.Text = Properties.Settings.Default.Version;
             textBox1.Text = Properties.Settings.Default.Destination;
-            filePath = textBox1.Text;
+            savePath = textBox1.Text;
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             Properties.Settings.Default.Version = versions.Text;
-            Properties.Settings.Default.Destination = filePath;
+            Properties.Settings.Default.Destination = savePath;
             Properties.Settings.Default.Save();
         }
 
@@ -53,8 +52,8 @@ namespace MapleDownloader
 
             if (folder.ShowDialog() == DialogResult.OK)
             {
-                filePath = folder.SelectedPath;
-                textBox1.Text = filePath;
+                savePath = folder.SelectedPath;
+                textBox1.Text = savePath;
             }
         }
 
@@ -80,34 +79,67 @@ namespace MapleDownloader
             c.DownloadProgressChanged += C_DownloadProgressChanged;
             c.DownloadFileCompleted += C_DownloadFileCompleted;
 
+            string fileName;
+
             if (versions.Text == "62") // special case
             {
-                fullName = "MSv62.rar";
-                c.DownloadFileAsync(new Uri("http://www.webgame.co.il/downloads/MapleStory_0.62.rar"), filePath + @"\" + fullName);
-                finishedDownload = true;
+                fileName = "MapleStory_V62.rar";
+                fullFilePath = savePath + @"\" + fileName;
+                c.DownloadFileAsync(new Uri("https://googledrive.com/host/0B8bVK378Rj_eZlp4X1A3Rllhblk/MapleStory_V62.rar"), fullFilePath);
+                finishedDownload = false;
+            }
+            else if(versions.Text == "83")
+            {
+                fileName = "MapleStory_V83.rar";
+                fullFilePath = savePath + @"\" + fileName;
+                c.DownloadFileAsync(new Uri("https://googledrive.com/host/0B8bVK378Rj_eZlp4X1A3Rllhblk/MapleStory_V83.rar"), fullFilePath);
+                finishedDownload = false;
             }
             else
             {
-                string fileName = baseName + versions.Text;
-                int numFiles = Directory.GetFiles(filePath, fileName + "*").Length;
+                fileName = baseName + versions.Text;
+                int numFiles = Directory.GetFiles(savePath, fileName + "*").Length;
+                string fullUrl;
 
                 if (numFiles == 0)
-                    fullName = fileName + ".exe";
+                    fileName += ".exe";
                 else
                 {
                     partNum = numFiles;
-                    fullName = fileName + ".z0" + partNum++;
+                    fileName += ".z0" + partNum++;
                 }
 
+                fullUrl = baseUrl + versions.Text + "/" + fileName;
+                fullFilePath = savePath + @"\" + fileName;
+
+                finishedDownload = false;
+
+                downloadFile(fullUrl, fileName);
+            }
+
+            if (!finishedDownload)
+            {
                 speed = new System.Timers.Timer(1000);
                 speed.Elapsed += Speed_Elapsed;
 
                 seconds = 1;
                 speed.Start();
 
-                finishedDownload = false;
+                button1.Enabled = false;
+                button2.Enabled = true;
+                versions.Enabled = false;
+                selectPath.Enabled = false;
 
-                downloadFile(baseUrl + versions.Text + "/" + fullName, fullName);
+                progressBar1.Show();
+
+                label3.Show();
+                label4.Text = fileName;
+                label4.Show();
+                label5.Show();
+                label6.Show();
+                label7.Show();
+                label8.Show();
+                label9.Show();
             }
         }
 
@@ -135,32 +167,36 @@ namespace MapleDownloader
             if (response != null && response.StatusCode == HttpStatusCode.OK)
             {
                 response.Close();
-                c.DownloadFileAsync(new Uri(url), filePath + @"\" + fileName);
-                
+                c.DownloadFileAsync(new Uri(url), fullFilePath);
 
-                button1.Enabled = false;
-                button2.Enabled = true;
-                versions.Enabled = false;
-                selectPath.Enabled = false;
-
-                progressBar1.Show();
-
-                label3.Show();
                 label4.Text = fileName;
-                label4.Show();
-                label5.Show();
-                label6.Show();
-                label7.Show();
-                label8.Show();
-                label9.Show();
             }
         }
 
         private void C_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
         {
-            progressBar1.Value = e.ProgressPercentage;
-            label6.Text = ((int)(e.BytesReceived / 10000.0)) / 100.0 + "  מתוך  " + ((int)(e.TotalBytesToReceive / 10000.0)) / 100.0 + "  מגה בייט";
-            label7.Text = e.ProgressPercentage + "%";
+            double fileSize = ((int)(e.TotalBytesToReceive / 10000.0)) / 100.0;
+            int progress = e.ProgressPercentage;
+
+            if (versions.Text == "62")
+            {
+                fileSize = 979.5;
+                progress = (int)(e.BytesReceived / 1000000 / fileSize * 100);
+                progressBar1.Value = progress;
+            }
+            else if (versions.Text == "83")
+            {
+                fileSize = 1594.67;
+                progress = (int)(e.BytesReceived / 1000000 / fileSize * 100 );
+                progressBar1.Value = progress;
+            }
+            else
+            {
+                progressBar1.Value = e.ProgressPercentage;
+            }
+
+            label6.Text = ((int)(e.BytesReceived / 10000.0)) / 100.0 + "  מתוך  " + fileSize + "  מגה בייט";
+            label7.Text = progress + "%";
             label9.Text = e.BytesReceived / seconds / 1000 + " קילו בייט / שנייה";
         }
 
@@ -170,22 +206,26 @@ namespace MapleDownloader
             {
                 c.Dispose();
 
-                if (File.Exists(filePath + @"\" + fullName))
+                if (File.Exists(fullFilePath))
                 {
                     this.ControlBox = false;
-                    File.Delete(filePath + @"\" + fullName);
+                    File.Delete(fullFilePath);
                     this.ControlBox = true;
                 }
             }
+            else if(versions.Text == "62" || versions.Text == "83")
+            {
+                finishedDownload = true;
+                MessageBox.Show("ההורדה הושלמה !", "", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign | MessageBoxOptions.RtlReading);
+            }
             else if (!finishedDownload)
             {
-                string fileName = baseName + versions.Text;
+                string fileName = baseName + versions.Text + ".z0" + partNum++;
+                string fullUrl = baseUrl + versions.Text + "/" + fileName;
 
-                fullName = fileName + ".z0" + partNum++;
+                fullFilePath = savePath + @"\" + fileName;
 
-                string url = baseUrl + versions.Text + "/" + fullName;
-
-                downloadFile(url, fullName);
+                downloadFile(fullUrl, fileName);
             }
         }
 
@@ -213,13 +253,15 @@ namespace MapleDownloader
             label7.Text = "0%";
             button2.Enabled = false;
             button1.Enabled = true;
+            versions.Enabled = true;
+            selectPath.Enabled = true;
             speed.Stop();
             finishedDownload = true;
         }
 
         private void enableDownloadButton(object sender, EventArgs e)
         {
-            if (versions.Text == "" || filePath == "")
+            if (versions.Text == "" || savePath == "")
                 return;
             else if (!button1.Enabled)
                 button1.Enabled = true;
@@ -263,6 +305,41 @@ namespace MapleDownloader
                     versions.Items.Add(ver);
                 }
                 ver++;
+            }
+        }
+
+        private void addSpecialVersions()
+        {
+            string[,] arr =
+            {
+                { "62", "https://googledrive.com/host/0B8bVK378Rj_eZlp4X1A3Rllhblk/MapleStory_V62.rar" },
+                { "83", "https://googledrive.com/host/0B8bVK378Rj_eZlp4X1A3Rllhblk/MapleStory_V83.rar" }
+            };
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                request = (HttpWebRequest)WebRequest.Create(arr[i, 1]);
+                request.Method = "HEAD";
+                request.Proxy = null;
+
+                response = null;
+
+                try
+                {
+                    response = (HttpWebResponse)request.GetResponse();
+                }
+                catch
+                {
+                }
+
+                if (response != null && response.StatusCode == HttpStatusCode.OK)
+                {
+                    response.Close();
+                    versions.Items.Add(arr[i, 0]);
+                }
             }
         }
 
